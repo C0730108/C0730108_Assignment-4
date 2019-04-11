24e3367c7165fcbfb666d7eb7fc1f4277fea672b
Jashandeepkaur <jashandeepkaur717@gmail.com>
2019-04-11 17:46:59 -0400

    Section E

--- C0730108_Assignment-4/Program.cs
+++ C0730108_Assignment-4/Program.cs
@@ -12,7 +12,9 @@ namespace C0730108_Assignment_4
     
     class Program
     {
-        
+
+
+      
         
             ArrayList Beowulf;
 
@@ -22,7 +24,10 @@ namespace C0730108_Assignment_4
                 p.Beowulf = new ArrayList();
                 p.Run();
                 p.ProcessArrayList();
-
+                string text = System.IO.File.ReadAllText("U:/Users/730108/Beowulf.txt");
+                p.FindNumberOfBlankSpaces(text);
+                p.FindNumberOfWords(text);
+                p.AverageNumberOfLetters(text);
             }
 
             public void Run()
@@ -35,8 +40,7 @@ namespace C0730108_Assignment_4
 
             public void ReadTextFiles()
             {
-                //Read file using StreamReader. Reads file line by line
-                using (StreamReader file = new StreamReader("U:/ Users/730108/Beowulf.txt"))
+                using (StreamReader file = new StreamReader("U:/Users/730108/Beowulf.txt"))
                 {
                     int counter = 0;
                     string ln;
@@ -95,8 +99,49 @@ namespace C0730108_Assignment_4
                 }
                 return false;
             }
+            public int FindNumberOfBlankSpaces(string line)
+            {
+                int countletters = 0;
+                int countSpaces = 0;
 
+                foreach (char c in line)
+                {
+                    if (char.IsLetter(c))
+                    {
+                        countletters++;
+                    }
+                    if (char.IsWhiteSpace(c))
+                    {
+                        countSpaces++;
 
+                    }
+
+                }
+                Console.WriteLine("Number of Blank Spaces: " + countSpaces);
+                Console.WriteLine("Number of letters: " + countletters);
+                return countletters;
+            }
+            public int FindNumberOfWords(string x)
+            {
+                int result = 0;
+                x = x.Trim();
+                if (x == "")
+                    return 0;
+                while (x.Contains("  "))
+                    x = x.Replace("  ", " ");
+                foreach (string y in x.Split(' '))
+                    result++;
+
+                Console.WriteLine("Result is " + result);
+                return result;
+
+            }
+            public double AverageNumberOfLetters(string text)
+            {
+                double average = ((text.Length - FindNumberOfBlankSpaces(text) / FindNumberOfWords(text)));
+                Console.WriteLine("Average is " + average);
+                return average;
+            }
         }
 
     }
